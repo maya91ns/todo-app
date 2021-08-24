708559a0459cc48478aa35df3cddbc2a81aa3216
Maja Skaljak <Maja.Skaljak@iwgplc.com>
2021-08-24 15:27:47 +0200

    todo-app first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { TodoListTableComponent } from './todo-list-table/todo-list-table.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
